eb511abc8a28ae7b7f68d34b2d355f226da31655
Kaveesha <kalshandhanasekara@gmail.com>
2025-04-09 12:35:36 +0530

    add scroll to top

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { Footer } from "./Footer";
 export { ProductCard } from "./ProductCard";
 export { Search } from "./Search";
 export { Rating } from "./Rating";
+export { ScrollToTop } from "./ScrollToTop";
