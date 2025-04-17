9cfa16e13b5aecb1d410a8318cd32982dd9a37ef
Kaveesha <kalshandhanasekara@gmail.com>
2025-04-17 08:52:09 +0530

    add prtected route

--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom';
 import {CartPage, HomePage, Login, ProductDetails, ProductsList, Register} from '../pages';
+import { ProtectedRoute } from './ProtectedRoute';
 
 export const AllRoutes = () => {
   return (
@@ -9,7 +10,7 @@ export const AllRoutes = () => {
             <Route path='/' element={<HomePage/>}></Route>
             <Route path='/products' element={<ProductsList/>}></Route>
             <Route path='/products/:id' element={<ProductDetails/>}></Route>
-            <Route path='/cart' element={<CartPage/>}></Route>
+            <Route path='/cart' element={<ProtectedRoute><CartPage/></ProtectedRoute>}></Route>
             <Route path='login' element={<Login/>}></Route>
             <Route path='register' element={<Register/>}></Route>
         </Routes>
